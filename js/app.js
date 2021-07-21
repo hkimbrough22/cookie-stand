327868a7ebc5b5177e59ce5ff6b94aaba2b1e4cc
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-07-20 20:35:42 -0700

    Adding total for all cookies

--- js/app.js
+++ js/app.js
@@ -43,7 +43,6 @@ addAShop('Tokyo', 3, 24, 1.2, ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am',
 addAShop('Dubai', 11, 38, 3.7, ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm']);
 addAShop('Paris', 20, 38, 2.3, ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm']);
 addAShop('Lima', 2, 16, 4.6, ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm']);
-console.log(allShops[1]);
 
 const shopDivElem = document.getElementById("shops");
 
@@ -94,16 +93,18 @@ function renderTableBody(){
 
 renderTableBody();
 const totalCookiesFinal = [];
+let totalCookiesAll = 0;
 
 function getTotals(){
   for (let i = 0; i < allShops[0].hourlyTotal.length; i++){
     let totalCookies = 0;
     for(let j = 0; j < allShops.length; j++){
       totalCookies = totalCookies + allShops[j].hourlyTotal[i];
+      totalCookiesAll = totalCookiesAll + allShops[j].hourlyTotal[i];
     }
     totalCookiesFinal.push(totalCookies);
   }
-  console.log(totalCookiesFinal);
+  totalCookiesFinal.push(totalCookiesAll);
 }
 getTotals();
 
@@ -113,6 +114,7 @@ function renderTableFoot(){
   for(let i = 0; i < allShops[0].hours.length; i++){
     let thElem = makeElement('th', trElem, totalCookiesFinal[i]);
   }
+  makeElement('th', trElem, totalCookiesAll);
 }
 renderTableFoot();
 
